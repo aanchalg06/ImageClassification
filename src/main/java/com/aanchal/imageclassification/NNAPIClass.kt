4350e369f2667f8e33054dc77be894928c933868
Aanchal Gupta <108565060+aanchalg06@users.noreply.github.com>
2024-04-20 16:06:16 +0530

    Update NNAPIClass.kt

--- src/main/java/com/aanchal/imageclassification/NNAPIClass.kt
+++ src/main/java/com/aanchal/imageclassification/NNAPIClass.kt
@@ -11,8 +11,7 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
 object NNAPIClass {
-    private const val MODEL_FILE_NAME = "model.tflite"
-    private const val NUM_CLASSES = 10
+
     val imageSize = 32
 
     init {
@@ -20,73 +19,31 @@ object NNAPIClass {
     }
 
 
-    fun classifyImage(context: Context, image: Bitmap, callback: (String) -> Unit) {
-
-        val interpreter = initializeInterpreter(context)
+    fun imagechecker(context: Context, image: Bitmap, callback: (String) -> Unit) {
 
 
-        val inputTensor = prepareInputTensor(image)
-
-
-        val outputTensor = TensorBuffer.createFixedSize(intArrayOf(1, NUM_CLASSES), DataType.FLOAT32)
+        val outr = TensorBuffer.createFixedSize(intArrayOf(1, NUM_CLASSES), DataType.FLOAT32)
         interpreter.run(inputTensor.buffer, outputTensor.buffer)
 
 
-        val confidenceArray = outputTensor.floatArray
-        var maxIndex = 0
-        var maxConfidence = confidenceArray[0]
-        for (i in 1 until confidenceArray.size) {
-            if (confidenceArray[i] > maxConfidence) {
-                maxConfidence = confidenceArray[i]
-                maxIndex = i
-            }
-        }
 
 
-        val classes = arrayOf("airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck")
-        val predictedClass = classes[maxIndex]
+        val class = arrayOf("airplane","automobile","bird","cat","deer","dog","frog","horse","ship","truck")
+        val predic = classes[maxIndex]
 
-        callback(predictedClass)
+        callback(predic)
 
 
-        interpreter.close()
+        inti.close()
     }
 
     private fun initializeInterpreter(context: Context): Interpreter {
-        val options = Interpreter.Options()
+        val opt = Interpreter.Options()
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val nnApiDelegate = NnApiDelegate()
-            options.addDelegate(nnApiDelegate)
-        }
-
-        return Interpreter(loadModelFile(context), options)
+    
+        return Interpreter(loadModelFile(context), opt)
     }
 
-    private external fun loadModelFile(assetManager: Any, modelFileName: String): ByteBuffer
-
-    private fun loadModelFile(context: Context): ByteBuffer {
-        return loadModelFile(context.assets, MODEL_FILE_NAME)
-    }
-
-    private fun prepareInputTensor(image: Bitmap): TensorBuffer {
-        val inputTensor = TensorBuffer.createFixedSize(intArrayOf(1, 32, 32, 3), DataType.FLOAT32)
-        val byteBuffer = ByteBuffer.allocateDirect(4 * 32 * 32 * 3).order(ByteOrder.nativeOrder())
 
 
-        val resizedImage = Bitmap.createScaledBitmap(image, 32, 32, true)
-
-
-        for (y in 0 until 32) {
-            for (x in 0 until 32) {
-                val pixel = resizedImage.getPixel(x, y)
-                byteBuffer.putFloat((pixel shr 16 and 0xFF) * (1f / 1))
-                byteBuffer.putFloat((pixel shr 8 and 0xFF) * (1f / 1))
-                byteBuffer.putFloat((pixel and 0xFF) * (1f / 1))
-            }
-        }
-
-        inputTensor.loadBuffer(byteBuffer)
-        return inputTensor
-    }
 }
